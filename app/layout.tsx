86265ee5495dc86f3a5beb08a7dc89a79077430d
antonioalt3000-7760 <antonioalt3000@users.noreply.github.com>
2026-03-11 08:16:19 +0000

    Add Google AdSense script ca-pub-7033623734141087

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-WKG7WCXZ');`,
           }}
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7033623734141087"
+          crossOrigin="anonymous"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 min-h-screen`}
