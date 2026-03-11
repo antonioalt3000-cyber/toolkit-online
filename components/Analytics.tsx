0c91fed6aa342bcb6b7d0891b23c50fac07d6dee
antonioalt3000-7760 <antonioalt3000@users.noreply.github.com>
2026-03-11 08:02:44 +0000

    Add Google Tag Manager GTM-WKG7WCXZ

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -2,26 +2,21 @@
 
 import Script from 'next/script';
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const GTM_ID = 'GTM-WKG7WCXZ';
 
 export default function Analytics() {
-  if (process.env.NODE_ENV !== 'production' || !GA_ID) {
+  if (process.env.NODE_ENV !== 'production') {
     return null;
   }
 
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}');
-        `}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
       </Script>
     </>
   );
